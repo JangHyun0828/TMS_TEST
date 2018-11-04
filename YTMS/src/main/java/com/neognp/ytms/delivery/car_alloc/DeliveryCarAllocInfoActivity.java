0e112d953d5e8206c28e264926ac778beee9f6cc
Android <neognp.com@gmail.com>
2018-11-04 09:18:44 +0900

    .

---
+++ YTMS/src/main/java/com/neognp/ytms/delivery/car_alloc/DeliveryCarAllocInfoActivity.java
@@ -0,0 +1,392 @@
+package com.neognp.ytms.delivery.car_alloc;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.neognp.ytms.R;
+import com.neognp.ytms.app.API;
+import com.neognp.ytms.app.Key;
+import com.neognp.ytms.carowner.car_alloc.CarAllocHistoryActivity;
+import com.neognp.ytms.delivery.car_loc.CarLocationActivity;
+import com.neognp.ytms.http.YTMSRestRequestor;
+import com.neognp.ytms.popup.LocationInfoDialog;
+import com.trevor.library.template.BasicActivity;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class DeliveryCarAllocInfoActivity extends BasicActivity {
+
+    private boolean onReq;
+
+    private Calendar curCal;
+
+    private boolean isListPullUp;
+
+    private ArrayList<Bundle> listItems = new ArrayList<Bundle>();
+    private ListAdapter listAdapter;
+
+    private Button curDateBtn;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView list;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delivery_car_alloc_info_activity);
+
+        curCal = Calendar.getInstance();
+        curCal.set(Calendar.HOUR_OF_DAY, 0);
+        curCal.set(Calendar.MINUTE, 0);
+        curCal.set(Calendar.SECOND, 0);
+        curCal.set(Calendar.MILLISECOND, 0);
+
+        setTitleBar("배차 정보", R.drawable.selector_button_back, 0, R.drawable.selector_button_refresh);
+
+        curDateBtn = (Button) findViewById(R.id.curDateBtn);
+        curDateBtn.setText(Key.SDF_CAL_WEEKDAY.format(curCal.getTime()));
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeColors(getResources().getIntArray(R.array.SwipeRefreshLayout_ColorScheme));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            public void onRefresh() {
+                search();
+            }
+        });
+
+        list = (RecyclerView) findViewById(R.id.list);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        list.setLayoutManager(layoutManager);
+        listAdapter = new ListAdapter();
+        list.setAdapter(listAdapter);
+
+        list.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                // CAUTION 아이템 높이가 RecycleView 높이보다 큰 경우, 아래 메서드 사용시 index가 모두 -1 로 반환되므로 사용 금지
+                //int firstItemIdx = layoutManager.findFirstCompletelyVisibleItemPosition();
+                //int lastItemIdx = layoutManager.findLastCompletelyVisibleItemPosition();
+                int firstItemIdx = layoutManager.findFirstVisibleItemPosition();
+                int lastItemIdx = layoutManager.findLastVisibleItemPosition();
+
+                Log.e(TAG, "+ onScrollStateChanged(): newState=" + newState + " / isListPullUp=" + isListPullUp + " / firstItemIdx=" + firstItemIdx + " / lastItemIdx=" + lastItemIdx);
+
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    if (!isListPullUp && firstItemIdx == 0) {
+                        Log.i(TAG, "+ onScrollStateChanged(): request refresh !");
+                    } else if (isListPullUp && lastItemIdx == listAdapter.getItemCount() - 1) {
+                        Log.i(TAG, "+ onScrollStateChanged(): request next list !");
+                        requestList(true);
+                    }
+                }
+
+                if (newState == RecyclerView.SCROLL_STATE_SETTLING) {
+                    int scrollY = recyclerView.computeVerticalScrollOffset();
+                    Log.e(TAG, "+ onScrollStateChanged(): scrollY=" + scrollY);
+                }
+            }
+
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+        list.setOnTouchListener(new View.OnTouchListener() {
+            float prevY;
+
+            public boolean onTouch(View v, MotionEvent event) {
+                //Log.i(TAG, "+ MotionEvent: " + event);
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    // pull down
+                    if (prevY < event.getY()) {
+                        isListPullUp = false;
+                    }
+                    // pull up
+                    else if (prevY > event.getY()) {
+                        isListPullUp = true;
+                    }
+                    prevY = event.getY();
+                }
+                return false;
+            }
+        });
+
+        init();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void init() {
+        try {
+            search();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick(View v) {
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(findViewById(android.R.id.content).getWindowToken(), 0);
+
+        switch (v.getId()) {
+            case R.id.titleLeftBtn0:
+                finish();
+                break;
+            case R.id.titleRightBtn1:
+                search();
+                break;
+            case R.id.prevDateBtn:
+                break;
+            case R.id.curDateBtn:
+                showCalendar();
+                break;
+            case R.id.nextDateBtn:
+                break;
+        }
+    }
+
+    private void showCalendar() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Log.e(TAG, "+ onDateSet(): year-month-day=" + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+
+                curCal.set(Calendar.YEAR, year);
+                curCal.set(Calendar.MONTH, monthOfYear);
+                curCal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                curDateBtn.setText(Key.SDF_CAL_WEEKDAY.format(curCal.getTime()));
+            }
+        }, curCal.get(Calendar.YEAR), curCal.get(Calendar.MONTH), curCal.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+
+    private void search() {
+        try {
+            // 오늘 날짜 이후로 설정 금지
+            //Calendar todayCal = Calendar.getInstance();
+            //todayCal.set(Calendar.HOUR_OF_DAY, 0);
+            //todayCal.set(Calendar.MINUTE, 0);
+            //todayCal.set(Calendar.SECOND, 0);
+            //todayCal.set(Calendar.MILLISECOND, 0);
+            //if (curCal.after(todayCal)) {
+            //    showSnackbar(R.drawable.ic_insert_invitation_black_24dp, "조회 종료일은 오늘 날짜 이후로 지정할 수 없습니다.");
+            //    return;
+            //}
+
+            requestList(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint ("StaticFieldLeak")
+    private synchronized void requestList(final boolean reqNextPage) {
+        if (onReq)
+            return;
+
+        try {
+            if (Key.getUserInfo() == null)
+                return;
+
+            final String fromDt = Key.SDF_PAYLOAD.format(curCal.getTime());
+
+            new AsyncTask<Void, Void, Bundle>() {
+                protected void onPreExecute() {
+                    onReq = true;
+                    if (reqNextPage)
+                        showLoadingDialog(null, false);
+                    else
+                        swipeRefreshLayout.setRefreshing(true);
+                }
+
+                protected Bundle doInBackground(Void... arg0) {
+                    JSONObject payloadJson = null;
+                    try {
+                        payloadJson = YTMSRestRequestor.buildPayload();
+                        payloadJson.put("userCd", Key.getUserInfo().getString("USER_CD"));
+                        payloadJson.put("fromDt", fromDt);
+
+                        // TEST
+                        payloadJson.put("userCd", "CAR0001");
+                        payloadJson.put("fromDt", "20180923");
+                        payloadJson.put("toDt", fromDt);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return YTMSRestRequestor.requestPost(API.URL_CAR_ALLOC_LIST, false, payloadJson, true, false);
+                }
+
+                protected void onPostExecute(Bundle response) {
+                    onReq = false;
+                    if (reqNextPage)
+                        dismissLoadingDialog();
+                    else
+                        swipeRefreshLayout.setRefreshing(false);
+
+                    try {
+                        Bundle resBody = response.getBundle(Key.resBody);
+                        String result_code = resBody.getString(Key.result_code);
+                        String result_msg = resBody.getString(Key.result_msg);
+
+                        if (result_code.equals("200")) {
+                            ArrayList<Bundle> data = resBody.getParcelableArrayList("data");
+                            addListItems(data);
+                        } else {
+                            showToast(result_msg + "(result_code:" + result_msg + ")", true);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        showToast(e.getMessage(), false);
+                    }
+
+                    // TEST
+                    //ArrayList<Bundle> list = new ArrayList<Bundle>();
+                    //for (int i = 0; i < 10; i++) {
+                    //    Bundle item = new Bundle();
+                    //    item.putString("FROM_CENTER_NM", "안성물류센터");
+                    //    item.putString("CAR_NO", "안성물류센터");
+                    //    list.add(item);
+                    //}
+                    //addListItems(list);
+                }
+            }.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized void addListItems(ArrayList<Bundle> items) {
+        if (items == null)
+            return;
+
+        try {
+            listItems.clear();
+            listItems.addAll(items);
+            listAdapter.notifyDataSetChanged();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        static final int TYPE_LIST_ITEM = 0;
+
+        public int getItemViewType(int position) {
+            return listItems.get(position).getInt("viewType", TYPE_LIST_ITEM);
+        }
+
+        public int getItemCount() {
+            return listItems.size();
+        }
+
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            RecyclerView.ViewHolder holder = null;
+
+            if (viewType == TYPE_LIST_ITEM) {
+                View v = View.inflate(getContext(), R.layout.delivery_car_alloc_info_item, null);
+                v.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
+                holder = new ListItemView(v);
+            }
+
+            return holder;
+        }
+
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+            ((ItemViewHolder) holder).onBindViewData(listItems.get(position));
+        }
+
+        abstract class ItemViewHolder extends RecyclerView.ViewHolder {
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+            }
+
+            public abstract void onBindViewData(final Bundle item);
+        }
+
+        class ListItemView extends DeliveryCarAllocInfoActivity.ListAdapter.ItemViewHolder {
+            public ListItemView(View itemView) {
+                super(itemView);
+            }
+
+            public void onBindViewData(Bundle item) {
+                try {
+                    // 상차지
+                    TextView dataTxt0 = ((TextView) itemView.findViewById(R.id.dataTxt0));
+                    String FROM_CENTER_NM = "상차지 : " + item.getString("FROM_CENTER_NM", "");
+                    SpannableString content = new SpannableString(FROM_CENTER_NM);
+                    content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+                    dataTxt0.setText(content);
+                    dataTxt0.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            LocationInfoDialog.show(DeliveryCarAllocInfoActivity.this, "상차지", item.getString("FROM_CENTER_ADDR", ""), item.getString("FROM_CENTER_EMP", ""), item.getString("FROM_CENTER_TEL", ""));
+                        }
+                    });
+
+                    // 차량번호
+                    TextView dataTxt1 = ((TextView) itemView.findViewById(R.id.dataTxt1));
+                    String CAR_NO = "차량번호 : " + item.getString("CAR_NO", "");
+                    content = new SpannableString(CAR_NO);
+                    content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+                    dataTxt1.setText(content);
+                    dataTxt1.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            Intent intent = new Intent(DeliveryCarAllocInfoActivity.this, CarLocationActivity.class);
+                            intent.putExtras((Bundle) item.clone());
+                            startActivity(new Intent(DeliveryCarAllocInfoActivity.this, CarLocationActivity.class));
+                        }
+                    });
+
+                    // 차량상태
+                    //((TextView) itemView.findViewById(R.id.dataTxt2)).setText("차량상태 : " + item.getString("", ""));
+
+                    // 도착예정
+                    //((TextView) itemView.findViewById(R.id.dataTxt3)).setText("도착예정 : " + item.getString("", ""));
+
+                    // 차주연결
+                    Button rightBtn0 = itemView.findViewById(R.id.palletsBtn);
+                    rightBtn0.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            //AppUtil.runCallApp("", true);
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
---
+++ YTMS/src/main/java/com/neognp/ytms/delivery/car_loc/CarLocationActivity.java
@@ -0,0 +1,145 @@
+package com.neognp.ytms.delivery.car_loc;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.neognp.ytms.R;
+import com.neognp.ytms.app.API;
+import com.neognp.ytms.app.Key;
+import com.neognp.ytms.http.YTMSRestRequestor;
+import com.trevor.library.template.BasicActivity;
+
+import org.json.JSONObject;
+
+public class CarLocationActivity extends BasicActivity {
+
+    private boolean onReq;
+    private Bundle args;
+
+    // private TextView
+    // private EditText
+    // private View
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.car_location_activity);
+
+        setTitleBar("차량 위치", R.drawable.selector_button_back, 0, R.drawable.selector_button_refresh);
+
+        //= findViewById(R.id.);
+
+        init();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void init() {
+        try {
+            args = getIntent().getExtras();
+            if (args == null)
+                return;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick(View v) {
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(findViewById(android.R.id.content).getWindowToken(), 0);
+
+        switch (v.getId()) {
+            case R.id.titleLeftBtn0:
+                finish();
+                break;
+            case R.id.titleRightBtn1:
+                requestCarLocation();
+                break;
+        }
+    }
+
+    @SuppressLint ("StaticFieldLeak")
+    private synchronized void requestCarLocation() {
+        if (onReq)
+            return;
+
+        try {
+            if (args == null)
+                return;
+
+            if (Key.getUserInfo() == null)
+                return;
+
+            //final String  = ((TextView) findViewById(R.id.)).getText().toString().trim();
+            //if (.isEmpty()) {
+            //    showToast("입력하세요.", true);
+            //    return;
+            //}
+
+            new AsyncTask<Void, Void, Bundle>() {
+                protected void onPreExecute() {
+                    onReq = true;
+                    showLoadingDialog(null, false);
+                }
+
+                protected Bundle doInBackground(Void... arg0) {
+                    JSONObject payloadJson = null;
+                    try {
+                        payloadJson = YTMSRestRequestor.buildPayload();
+                        //payloadJson.put("", );
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return YTMSRestRequestor.requestPost(API.URL_, false, payloadJson, true, false);
+                }
+
+                protected void onPostExecute(Bundle response) {
+                    onReq = false;
+                    dismissLoadingDialog();
+
+                    try {
+                        Bundle resBody = response.getBundle(Key.resBody);
+                        String result_code = resBody.getString(Key.result_code);
+                        String result_msg = resBody.getString(Key.result_msg);
+
+                        if (result_code.equals("200")) {
+
+                        } else {
+                            showToast(result_msg + "(result_code:" + result_msg + ")", true);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        showToast(e.getMessage(), false);
+                    }
+                }
+            }.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showActivity(Bundle args) {
+        if (args == null)
+            return;
+
+        // Intent intent = new Intent(this, Activity.class);
+        // intent.putExtra(LibKey.args, args);
+        // startActivityForResult(intent, REQUEST_);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
